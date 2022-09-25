442dd4a08981ac2259efe92008bd05b7e098b15f
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-25 10:26:18 +0600

    set item local storage

--- src/utilities/localStorageManage.js
+++ src/utilities/localStorageManage.js
@@ -1,12 +1,40 @@
 const setDataToLocalStorage = (id) => {
-    const quantity = localStorage.getItem(id)
+    // =======> for basic learning <============
+    //==========================================
+    // const quantity = localStorage.getItem(id)
+    // if (quantity) {
+    //     let newQuantity = parseInt(quantity) + 1;
+    //     //---- // let newQuantity = +quantity + 1;
+    //     localStorage.setItem(id, newQuantity)
+    // } else {
+    //     localStorage.setItem(id, 1)
+    // }
+
+    // =============> advanced way <=================
+    //===============================================
+    const shoppingCart = {};
+
+    const quantity = shoppingCart[id];
     if (quantity) {
-        let newQuantity = parseInt(quantity) + 1;
-        // let newQuantity = +quantity + 1;
-        localStorage.setItem(id, newQuantity)
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
     } else {
-        localStorage.setItem(id, 1)
+        shoppingCart[id] = 1;
     }
+
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
+
+
+
+
+    // localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
+    // const quantity = localStorage.getItem(id)
+    // if (quantity) {
+    //     let newQuantity = parseInt(quantity) + 1;
+    //     // localStorage.setItem(id, newQuantity)
+    // } else {
+    //     // localStorage.setItem(id, 1)
+    // }
 
 }
 
